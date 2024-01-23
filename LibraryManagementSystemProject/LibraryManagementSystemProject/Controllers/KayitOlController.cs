b590bb29557d0c61443984e1db86d7686b634a13
eslemmegreli <eslemmegreli@gmail.com>
2024-01-23 09:26:50 +0300

    Library Management System
    Asp.NET MVC ile programlanmıştır.

---
+++ LibraryManagementSystemProject/LibraryManagementSystemProject/Controllers/KayitOlController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LibraryManagementSystemProject.Models.Entity;
+
+namespace LibraryManagementSystemProject.Controllers
+{
+    [AllowAnonymous]   //Authentication işlemini devre dışı bırakır
+    public class KayitOlController : Controller
+    {
+
+        dbKUTUPHANEEntities db = new dbKUTUPHANEEntities();
+        // GET: KayitOl
+
+        [HttpGet]
+        public ActionResult Kayit()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Kayit(TBLUYELER p)
+        {
+            if (!ModelState.IsValid)   //model geçerli değilse (yani, geçerlilik kontrollerinden biri başarısız olduysa)
+            {
+                return View("Kayit");  //kayit'a dön
+            }
+            db.TBLUYELER.Add(p);
+            db.SaveChanges();
+            return View();
+        }
+    }
+}
